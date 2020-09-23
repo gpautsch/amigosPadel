62d975865bf2b6bad44e333e66163e014b481382
gpautsch <germanpautsch@gmail.com>
2020-09-23 09:42:56 -0300

    Pruebas Inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'amigosPadel';
+  title:string = 'Amigos del Padel';
+  //show:boolean = true; // DESCOMENTAR PARA QUE FUNCIONE PARA ej1 y ej 2
+  // LA LINEA DE ABAJO ES SOLO PARA MOSTRAR FUNCIONAM
+  // cursos:string[]=['HTML', 'ANGULAR', 'PHP'];
+  // AQUI SE CREA UNA LISTA DE TIPO OBJETO (any)
+  animales:Array<any> = [
+    {tipo:'Perro', nombre:'Aira', edad:7},
+    {tipo:'Gato', nombre:'Moana', edad:4},
+    {tipo:'Perro', nombre:'Uma', edad:6}
+  ]
 }
