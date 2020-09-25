e5e5d35db3409c23a1b429d8d90e1c33e1a90d7e
gpautsch <germanpautsch@gmail.com>
2020-09-25 11:46:50 -0300

    prueba readme.txt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
 import { FooterComponent } from './footer/footer.component';
+import { CuerpoComponent } from './cuerpo/cuerpo.component';
+import { Cuerpo2Component } from './cuerpo2/cuerpo2.component';
+import { ContactoComponent } from './contacto/contacto.component';
+
+//LUEGO ESTA CONSTANTE SE UTILIZA EN EL IMPORT
+const routes: Routes = [
+  { path: 'contacto', component: ContactoComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     CabeceraComponent,
-    FooterComponent
+    CuerpoComponent,
+    FooterComponent,
+    Cuerpo2Component,
+    ContactoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes) 
   ],
   providers: [],
   bootstrap: [AppComponent]
